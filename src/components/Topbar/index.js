6b090ffe2235dea888c2748d83da6c51cfab37f8
wcartagenaasc <wesleycartagena23@gmail.com>
2022-07-05 22:10:32 -0400

    test

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -25,9 +25,6 @@ const Topbar = () => {
                 <NavLink exact="true" activeclassname="active" id="contact-link" to="contact">
                     <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" id="contact-link" to="contact">
-                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
-                </NavLink>
             </nav>
             <div class="Social-List">
             <ul id="horizontal-list">
