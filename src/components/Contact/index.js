5fdfa519354a646605acbe505d89c48bde136fe5
wcartagenaasc <wesleycartagena23@gmail.com>
2022-07-12 22:26:47 -0500

    Contact-Form-Draft

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -11,6 +11,27 @@ const Contact = () => {
     }, 4000)
     return () => clearTimeout()
     }, []);
+    const [status, setStatus] = useState("Submit");
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setStatus("Sending...");
+      const { name, email, message } = e.target.elements;
+      let details = {
+        name: name.value,
+        email: email.value,
+        message: message.value,
+      };
+      let response = await fetch("http://localhost:5000/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(details),
+      });
+      setStatus("Submit");
+      let result = await response.json();
+      alert(result.status);
+    };
     return(
         <div className ='container contact-page'>
             <div className='text-zone'>
@@ -26,8 +47,24 @@ const Contact = () => {
                     New York,
                     <br />
                     <span>wesley.cartagena23@outlook.com</span>
-                </div>                
+                </div>
+                                
             </div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" required />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" required />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea id="message" required />
+                </div>
+                <button type="submit">{status}</button>
+            </form>
         </div>
     );
 }
